3dfd1fe8ecb72eb4da9f00bd634babfa9b18084c
nikitaenmi <nikita_turchaninovv@mail.ru>
2024-03-17 15:20:08 +0300

    the4

--- Database/DAL/BrandDAL.py
+++ Database/DAL/BrandDAL.py
@@ -1,5 +1,6 @@
 from Database.Models.Brand import Brand
 from Database.config import Session
+from sqlalchemy import text
 class BrandDAL:
     @staticmethod
     def new_brand(namebrand):
@@ -13,6 +14,23 @@ class BrandDAL:
         with Session() as session:
             session.query(Brand).filter(Brand.id_brand == 14).delete()
             session.commit()
+
+    @staticmethod
+    def search_brand(name):
+        with Session() as session:
+            res = session.execute(text("SELECT * FROM brand"))
+            for i in range(len(res.all())):
+                res = session.execute(text("SELECT * FROM brand"))
+                if res.all()[i][1] == name:
+                    return True, print("True")
+                else:
+                    return False, print("False")
+
+
+
+
+
+
 
 
 
